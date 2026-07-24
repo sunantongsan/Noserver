dcebac6592a688636dfecfc822cb9202546e36f5
sunantongsan <104156645+sunantongsan@users.noreply.github.com>
2026-07-24 09:39:04 +0700

    build: bump version to 10.0

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -17,8 +17,8 @@ android {
     applicationId = "com.aistudio.livingmesh.pvskqn"
     minSdk = 24
     targetSdk = 36
-    versionCode = 7
-    versionName = "7.0"
+    versionCode = 10
+    versionName = "10.0"
 
     testInstrumentationRunner = "androidx.test.runner.AndroidJUnitRunner"
   }
